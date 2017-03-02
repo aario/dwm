1f6e7454d178e823bee1b547e5018cef37c6184e
Aario Shahbany <aario.shahbany@bandist.com>
2017-03-02 18:23:22 +0100

    Applied push patch and updated TODO file

--- config.def.h
+++ config.def.h
@@ -99,6 +99,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_x,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_j,      pushdown,       {0} },
+	{ MODKEY|ControlMask,           XK_k,      pushup,         {0} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_Left,      setmfact,       {.f = -0.05} },
